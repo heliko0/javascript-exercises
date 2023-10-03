63e92fa5f4d6ddf67fa627e9038f3d0a5836ab5c
heliko0 <144160130+heliko0@users.noreply.github.com>
2023-10-03 19:08:32 +0100

    Amended code to fit subsequent exercises
    Changed from using push statement to create new array -> amending
    original array with splice, preventing repeated value adds.
    
    New code loops through to finds the index (position) value of given
    removes, and splices value by 1, and returns the amended array.
    
    Added if statement to check for non present values.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function (ary, ...removes) {
+    // return ary.filter(section => section !== remove) The filter() method processes each element in the ary array and includes only the elements for which the callback function returns true. Elements that are equal to the remove value are filtered out (excluded).
 
+    //     let newAry = [];
+
+    //     for (value of ary) {
+    //         for (remove of removes){
+    //         if (value !== remove)
+    //         newAry.push(value);
+    //         }
+    //     }
+
+    //     return newAry;
+    // };
+
+    for (remove of removes) {
+        const removeIndex = ary.indexOf(remove);
+        if (removeIndex >= 0) ary.splice(removeIndex, 1);  // splice takes 2 parameters, position and number of values to remove. needs if statement to ignore non present vals
+    }
+
+    return ary;
 };
 
 // Do not edit below this line
