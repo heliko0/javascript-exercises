94645ef329087619fc6de293360f225029504b62
heliko0 <144160130+heliko0@users.noreply.github.com>
2023-10-03 18:29:51 +0100

    Corrected code
    Included comments detailing explanation for code working

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let newString = []; //creates empty array for reversed letters to be added
+    let seperate = string.split(""); //splits the given string into an array with individual letters
 
+    for (let i = 0; i <= seperate.length; i++) { //i for each position up to the string length
+        newString.unshift(seperate[i]); //array newString the existing letter in position i added to the front, reversing the string
+                                        
+    }
+
+    return newString.join(""); //once all instances finished, joins the array with no spaces. toString leaves commas.
 };
+
+// const reverseString = function (string) {            given solution, when did they mention reverse() though?
+//     return string.split("").reverse().join("");
+//   };     
+  
 
 // Do not edit below this line
 module.exports = reverseString;
