e0f1a7a1346a9c8152c864701fbaa5a63a1cb5f5
heliko0 <144160130+heliko0@users.noreply.github.com>
2023-10-03 18:28:08 +0100

    Amended repeatString exercise to be correct
    Included alternative and given methods in comments

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+    let output = "";
+    if (times < 0) return "ERROR";
+    for (let i = 1; i <= times; i++) {
+        output += word;
+    }
+    return output;
 };
 
 // Do not edit below this line
