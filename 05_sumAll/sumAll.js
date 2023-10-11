02b8e83858d81af41b233d11ee876b4956fb5911
heliko0 <144160130+heliko0@users.noreply.github.com>
2023-10-11 19:38:03 +0100

    First version of successful code
    Code passes all tests, but planned to optimise and clean

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    let total = 0;
+    let left = a;
+    let right = b;
 
+    if (left > right) {
+        right = a;
+        left = b;
+    }
+
+    if (left < 0 || right < 0 || Number.isInteger(left) === false || Number.isInteger(right) === false) return "ERROR";
+
+    for (i = left; i <= right; i++) {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
