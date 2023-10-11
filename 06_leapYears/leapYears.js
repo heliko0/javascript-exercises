44171bb0248039f9207ac3b848eec6529e430290
heliko0 <144160130+heliko0@users.noreply.github.com>
2023-10-11 20:08:08 +0100

    Added cleaner code and commented for self aid

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,13 @@
 const leapYears = function(year) {
-    if (year % 400 === 0) return true;
-    // if year divisible by 4 and not a century year
-    else if (year % 4 === 0 && year % 100 !== 0) return true;
-    else return false;
+    // if (year % 400 === 0) return true;
+    // // if year divisible by 4 and not a century year
+    // else if (year % 4 === 0 && year % 100 !== 0) return true;
+    // else return false;
+
+
+    // return boolean result of (year divisible by 4 AND (year divisible by 400 OR year not a century year))
+    // e.g. 1996 - divisible by 4 and not a century, 2000 - divisible by 4 and 400
+    return year % 4 === 0 && (year % 400 === 0 || year % 100 !== 0);
 };
 
 // Do not edit below this line
