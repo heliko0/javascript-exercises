4bef9fc97e7f554ada21e407a8c3bb92ed1859fe
heliko0 <144160130+heliko0@users.noreply.github.com>
2023-10-11 20:04:18 +0100

    Created first version of successful code

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 400 === 0) return true;
+    // if year divisible by 4 and not a century year
+    else if (year % 4 === 0 && year % 100 !== 0) return true;
+    else return false;
 };
 
 // Do not edit below this line
